d0660ce0cc39c6c3bf83777dbf68a03c2770fc2e
Rafael Estevam <rafael.estevam@exedconsulting.com>
2018-06-28 18:02:26 -0300

    Formulário de Cadastro de Empresas

--- src/main/java/br/com/intelliapps/digitalsutor/controllers/EmpresaController.java
+++ src/main/java/br/com/intelliapps/digitalsutor/controllers/EmpresaController.java
@@ -1,10 +1,14 @@
 package br.com.intelliapps.digitalsutor.controllers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.intelliapps.digitalsutor.models.Empresa;
 import br.com.intelliapps.digitalsutor.models.TipoAliq;
@@ -21,13 +25,22 @@ public class EmpresaController {
 	public String empresaForm(Empresa empresa, Model model) {
 		
 		Usuario usuario = usuarioLogadoService.usuarioLogado();
+		Set<Usuario> usuarios = new HashSet<Usuario>();
+		usuarios.add(usuario);
+		empresa.setUsuarios(usuarios);
 		
 		model.addAttribute("nome", usuario.getNome());
 		model.addAttribute("ultimonome", usuario.getUltimonome());
-		
+		model.addAttribute("empresa", empresa);
 		model.addAttribute("tipoAliq", TipoAliq.values());
 		
 		return "empresa";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/empresa", method=RequestMethod.POST)
+	public String criaEmpresa(Empresa empresa) {
+		return "Logo mais, você criará a sua empresa!";
 	}
 	
 	
